fa2119bb917d11bbe68bd01397cf043311f1e73f
毛 <1296193245@qq.com>
2022-03-29 12:54:07 +0800

    Python_custom_module

---
+++ test1.py
@@ -0,0 +1,28 @@
+"""
+ * Project name(项目名称)：Python自定义模块
+ * Package(包名): 
+ * File(文件名): test1
+ * Author(作者）: mao
+ * Author QQ：1296193245
+ * GitHub：https://github.com/maomao124/
+ * Date(创建日期)： 2022/3/29 
+ * Time(创建时间)： 12:50
+ * Version(版本): 1.0
+ * Description(描述)： 
+ """
+
+str1 = "test"
+
+
+def f1():
+    print("hello world")
+
+
+def add(a, b):
+    """
+    add
+    :param a: a
+    :param b: a
+    :return: a+b
+    """
+    return a + b
---
+++ test2.py
@@ -0,0 +1,18 @@
+"""
+ * Project name(项目名称)：Python自定义模块
+ * Package(包名): 
+ * File(文件名): test2
+ * Author(作者）: mao
+ * Author QQ：1296193245
+ * GitHub：https://github.com/maomao124/
+ * Date(创建日期)： 2022/3/29 
+ * Time(创建时间)： 12:52
+ * Version(版本): 1.0
+ * Description(描述)： 
+ """
+import test1
+
+if __name__ == '__main__':
+    print(test1.str1)
+    test1.f1()
+    print(test1.add(2, 7))
