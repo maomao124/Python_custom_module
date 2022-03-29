7bf207d7230974a36fb5f5791584598162d51161
毛 <1296193245@qq.com>
2022-03-29 13:08:43 +0800

    Python_custom_module

---
+++ 2t.py
@@ -0,0 +1,22 @@
+"""
+ * Project name(项目名称)：Python自定义模块
+ * Package(包名): 
+ * File(文件名): 2t
+ * Author(作者）: mao
+ * Author QQ：1296193245
+ * GitHub：https://github.com/maomao124/
+ * Date(创建日期)： 2022/3/29 
+ * Time(创建时间)： 13:03
+ * Version(版本): 1.0
+ * Description(描述)： 
+ """
+
+
+def add(a, b):
+    """
+    add
+    :param a: a
+    :param b: a
+    :return: a+b
+    """
+    return a + b
---
+++ test3.py
@@ -0,0 +1,20 @@
+"""
+ * Project name(项目名称)：Python自定义模块
+ * Package(包名): 
+ * File(文件名): test3
+ * Author(作者）: mao
+ * Author QQ：1296193245
+ * GitHub：https://github.com/maomao124/
+ * Date(创建日期)： 2022/3/29 
+ * Time(创建时间)： 12:56
+ * Version(版本): 1.0
+ * Description(描述)： Python __import__()函数引入模块名
+ """
+
+# import test f
+# import 2t
+__import__("test f")
+__import__("2t")
+
+if __name__ == '__main__':
+    print()
